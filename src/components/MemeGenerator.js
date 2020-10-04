8fc3f70cfa5c68dffaeb0b19303a1f8e187f51b2
sedx876 <swatkins876@gmail.com>
2020-10-04 10:21:32 -0400

    added MemeGenerator component

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {}; //empty state
+  }
+  render() {
+    return <h1>MEME GENERATOR SECTION</h1>;
+  }
+}
+export default MemeGenerator;
